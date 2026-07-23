3dc643defac21a08486cd52cebf07d3dde84ce76
Magd <amagd6206@gmail.com>
2026-07-23 19:45:11 +0300

    Fix: merge frontend and backend routing

--- src/middlewares/limiter.js
+++ src/middlewares/limiter.js
@@ -1,13 +1,15 @@
 const rateLimit = require("express-rate-limit");
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
-  message: "Too many requests from this IP, please try again after 15 minutes",
+  limit: 1000, // حد عام ضخم جداً
+  message: "Too many requests from this IP, please try again later",
 });
+
 const limiterLogin = rateLimit({
-  windowMs: 60 * 60 * 1000, // 15 minutes
-  limit: 3, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
-  message: "Too many requests from this IP, please try again after 15 minutes",
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 1000, // السماح بـ 10,000 محاولة تسجيل دخول قبل أي حظر
+  message: "Too many login attempts, please try again later",
 });
 
 module.exports = {
